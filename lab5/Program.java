e55cd7bde5052551beab9016aaef883925eb5bde
Victor Shulpov <vshulpov@list.ru>
2020-10-14 00:09:08 +0700

    static accociation is working

--- lab5/Program.java
+++ lab5/Program.java
@@ -5,9 +5,26 @@ public class Program{
 	//System.out.println("Hi, Kate!");
 	Car bmw_x6 = new Car();
 	Engine bmw_engine = new Engine();
-	bmw_x6.init("BMW X6", 1200000, "Red", 0, 0, bmw_engine);
+	bmw_engine.init(0, 4395, 625, 8);
+	bmw_x6.init("BMW X6", 3500000, "BLACK", 0, 0, bmw_engine); //инициализируем поля объекта
 	bmw_x6.displayDataCar();
-	bmw_x6.readCarData();
+	bmw_x6.displayDataCar();
+	bmw_x6.startEngine(); //пытаемся завести двигатель
+	bmw_x6.displayDataCar();
+	bmw_x6.addBenzine(10); //добавляем бензин
+	bmw_x6.displayDataCar();
+	bmw_x6.startEngine(); //снова пытаемся завести двигатель
+	bmw_x6.displayDataCar();
+	for (int i = 0; i < 4; i++) {
+		bmw_x6.addSpeed(i * 5); //добавляем скорость
+		bmw_x6.displayDataCar();
+	}
+	for (int i = 0; i < 4; i++) {
+		bmw_x6.reduceSpeed(i * 5); //убавляем скорость
+		bmw_x6.displayDataCar();
+	}
+
+	bmw_x6.stopEngine(); //останавливаем двигатель
 	bmw_x6.displayDataCar();
 	}
 }
@@ -73,34 +90,51 @@ class Car {
 	}
 	void readCarData(){
 		System.out.println("ENTER CAR DATA:");
+		
 		System.out.print("\tname:\t");
 		this.name = in.nextLine();
+
+
 		System.out.print("\tprice:\t");
 		this.price = in.nextInt();
 		in.nextLine();//очистка потока
+		
 		System.out.print("\tcolor:\t");
 		this.color = in.nextLine();
-		/*System.out.print("\tengineRPM:\t");
-		this.engineRPM = in.nextInt();
+
+		System.out.print("\tengineRPM:\t");
+		this.engine.setEngineRPM(in.nextInt());
+		in.nextLine();//очистка потока
+		
 		System.out.print("\tcapacity:\t");
-		this.capacity = in.nextInt();
+		this.engine.setCapacity(in.nextInt());
+		in.nextLine();//очистка потока
+		
 		System.out.print("\tengine power:\t");
-		this.engine = in.nextInt();
-		*/
+		this.engine.setEnginePower(in.nextInt());
+		in.nextLine();//очистка потока
+		
+		System.out.print("\tquantity of cylinders:\t");
+		this.engine.setQuantityOfCylinders(in.nextInt());
+		in.nextLine();//очистка потока
+
 		System.out.print("\tspeed:\t");
 		this.speed = in.nextInt();
 		in.nextLine();//очистка потока
+
 		System.out.print("\tbenzine:\t");
 		this.benzine = in.nextInt();
 		in.nextLine();//очистка потока
-
 	}
 	void displayDataCar(){
-		System.out.println("\tCar data");
+		System.out.println("\n\tCAR DATA");
 		System.out.println("\t\tName:\t" + this.name);
 		System.out.println("\t\tPrice:\t" + this.price);
 		System.out.println("\t\tColor:\t" + this.color);
-		//
+		System.out.println("\t\tEngineRPM:\t" + this.engine.getEngineRPM());
+		System.out.println("\t\tCapacity:\t" + this.engine.getCapacity());
+		System.out.println("\t\tEngine Power:\t" + this.engine.getEnginePower());
+		System.out.println("\t\tQuanity of cylinders:\t" + this.engine.getQuantityOfCylinders());
 		System.out.println("\t\tBenzine:\t" + this.benzine);
 		System.out.println("\t\tSpeed:\t" + this.speed);
 	}
@@ -109,16 +143,40 @@ class Car {
 	this.benzine += liters;
 	}
 	void startEngine(){
-		
+		if (this.benzine > 0) {
+			this.engine.setEngineRPM(800);
+			System.out.println("Engine started!");
+		}
+		else {
+			System.out.println("No benzine. Engine didn't start!");
+		}
 	}
 	void stopEngine(){
-	
+		if (this.engine.getEngineRPM() > 0) {
+			this.engine.setEngineRPM(0);
+			System.out.println("Engine stopped!");
+		}
+		else {
+			System.out.println("Engine stopped already!");
+		}
 	}
 	void addSpeed(int speed){
-	
+		if (this.engine.getEngineRPM() > 0) {
+			this.speed += speed;
+			System.out.println("Car speeded up!");
+		}
+		else {
+			System.out.println("Engine isn't starting. Car didn't speed up!");
+		}
 	}
 	void reduceSpeed(int speed){
-	
+		if (this.speed > 0) {
+			this.speed -= speed;
+			System.out.println("Car speeded down!");
+		}
+		else {
+			System.out.println("Car is parking. Car didn't speed down!");
+		}
 	}
 }
 /*
