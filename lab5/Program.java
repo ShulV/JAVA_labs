1169e9d288fa1ca5a326492c00a3755666840b8e
Victor Shulpov <vshulpov@list.ru>
2020-10-13 23:11:09 +0700

    car class methods

---
+++ lab5/Program.java
@@ -0,0 +1,46 @@
+public class Program{
+	public static void main(String[] args) {
+
+	System.out.println("Hi, Kate!");
+	Car bmw_x6 = new Car();
+	}
+}
+
+
+
+class Engine{
+	
+}
+
+class Car {
+	void init(String name, int price, String color, int speed, int benzine, Engine engine){
+	
+	}
+	void readCarData(){
+	
+	}
+	void displayDataCar(){
+	
+	}
+	void addBenzine(int liters){
+	
+	}
+	void startEngine(){
+	
+	}
+	void stopEngine(){
+	
+	}
+	void addSpeed(int speed){
+	
+	}
+	void reduceSpeed(int speed){
+	
+	}
+}
+/*
+         javac Car.java - компилируем программа (идЄт проверка синтаксиса, семантики, найдутс€ ошибки, если они есть)
+		 в случае удачной компил€ции создает файл Car.class (байт код)
+
+		 java Car - запускает консольную программу
+*/
